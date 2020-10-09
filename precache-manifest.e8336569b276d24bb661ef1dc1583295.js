4686511ad548fecb0d1d1a0bf5db6f7d50c01fc4
Robert Marasescu <robert.marasescu96@gmail.com>
2020-10-09 11:17:42 +0300

    Updates

---
+++ precache-manifest.e8336569b276d24bb661ef1dc1583295.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf06e45f96836cc259da4cd3815841ed",
+    "url": "/react-spotify-lyrics/index.html"
+  },
+  {
+    "revision": "39981359d449159e3093",
+    "url": "/react-spotify-lyrics/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "a827a8c950346c9c2efa",
+    "url": "/react-spotify-lyrics/static/css/main.816e2c00.chunk.css"
+  },
+  {
+    "revision": "39981359d449159e3093",
+    "url": "/react-spotify-lyrics/static/js/2.294e6263.chunk.js"
+  },
+  {
+    "revision": "4f41e551454b7ceb5ea520e07b47f17e",
+    "url": "/react-spotify-lyrics/static/js/2.294e6263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a827a8c950346c9c2efa",
+    "url": "/react-spotify-lyrics/static/js/main.26241165.chunk.js"
+  },
+  {
+    "revision": "22fbede7cc7e8a5b7463",
+    "url": "/react-spotify-lyrics/static/js/runtime-main.39ae610f.js"
+  }
+]);
